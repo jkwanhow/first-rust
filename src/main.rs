c4ad8ef63b15933555e61185b7e3045bb1f57c3a
jkwanhow <58902790+jkwanhow@users.noreply.github.com>
2023-04-06 01:08:53 +1000

    made letter counted with heavy help from docs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
 use std::fs;
-// plan is to generate a huffman tree while reading a text file
+use std::collections::HashMap;
+
+// plan is to generate a huffman tree
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut occurance_of_letter= HashMap::new();
 
     let file_path = &args[1];
 
@@ -11,6 +14,14 @@ fn main() {
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
+    for c in contents.chars() {
+        // get current value and increment by 1 or else insert 1.
+        occurance_of_letter.entry(c).and_modify(|counter| *counter += 1).or_insert(1);
+        let current_value = *occurance_of_letter.get_mut(&c).unwrap();
+        println!("{} {}", c, current_value);
+    }
+
+    // plan next is to iterate letters then inplace add them into a tree or an array
     println!("With text:\n{contents}");
     
 }
